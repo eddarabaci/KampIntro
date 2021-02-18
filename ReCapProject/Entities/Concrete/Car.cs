554fc3f2b7cf737c1ce495ffd591514158fc6086
Edda <Edda@DESKTOP-A704ONG>
2021-02-18 13:30:28 +0300

    comment added

--- ReCapProject/Entities/Concrete/Car.cs
+++ ReCapProject/Entities/Concrete/Car.cs
@@ -7,9 +7,9 @@ namespace Entities.Concrete
 {
     public class Car : IEntity
     {
-        public int Id { get; set; }
-        public int BrandId { get; set; }
-        public int ColorId { get; set; }
+        public int Id { get; set; } //primary key
+        public int BrandId { get; set; } //foreign key
+        public int ColorId { get; set; } //foreign key
         public string ModelYear { get; set; }
         public decimal DailyPrice { get; set; }
         public string Descriptions { get; set; }
