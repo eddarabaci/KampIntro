efbf80d35fa328ac25634b77b0c4c445969bd1ab
Edda <Edda@DESKTOP-A704ONG>
2021-03-08 17:46:07 +0300

    WebAPI layer created

--- MyFinalProject/Business/Concrete/ProductManager.cs
+++ MyFinalProject/Business/Concrete/ProductManager.cs
@@ -37,10 +37,10 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
             //business codes
-            if (DateTime.Now.Hour == 22) 
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour == 20)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
         }
 
---
+++ MyFinalProject/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]  //Attribute (C#) or Annotation (Java)
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //IoC Container -- Inversion of Control
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll() 
+        {
+            var result = _productService.GetAll();
+            if (result.Success) 
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id) 
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product) 
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
